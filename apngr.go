3d28623af3e0ee2f39d3ab38f4fa7ee020cfd38b
kts of kettek (yan) <kts@kettek.net>
2021-12-29 06:12:29 -0800

    Update copyright date

--- apngr.go
+++ apngr.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 Ketchetwahmeegwun T. Southall / kts of kettek. All Rights reserved.
+Copyright 2018-2022 Ketchetwahmeegwun T. Southall / kts of kettek. All Rights reserved.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
